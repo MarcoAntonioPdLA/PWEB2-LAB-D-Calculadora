d7df40db4a4a9117347ed5928115f6f61a2956c8
MarcoAntonioPonceDeLeon <mponcedeleona@unsa.edu.pe>
2021-04-19 19:31:52 -0500

    Plantilla base

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+  public int add(int a, int b){
+    return 0;
+  }
+  public int sub(int a, int b){
+    return 0;
+  }
+  public int mul(int a, int b){
+    return 0;
+  }
+  public int div(int a, int b){
+    return 0;
+  }
+  public int mod(int a, int b){
+    return 0;
+  }
+}
