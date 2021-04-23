71de462e3041e45ad3c6c9bccf5fcabfa42346ba
MarcoAntonioPonceDeLeon <mponcedeleona@unsa.edu.pe>
2021-04-22 20:02:31 -0500

    Mejorando la función div

--- Calculator.java
+++ Calculator.java
@@ -1,17 +1,23 @@
 public class Calculator {
   public int add(int a, int b){
-    return a+b;
+    return a + b;
   }
   public int sub(int a, int b){
-    return a-b;
+    return a - b;
   }
   public int mul(int a, int b){
-    return a*b;
+    return a * b;
   }
   public int div(int a, int b){
-    return a/b;
+    try {
+      return a / b;
+    }
+    catch(Exception e) {
+      System.out.println("Error de división por cero");
+      return 0;
+    } 
   }
   public int mod(int a, int b){
-    return a%b;
+    return a % b;
   }
 }
